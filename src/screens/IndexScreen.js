51520db6a81b7b63740450da078098b4b2cdeb4c
LingyuLiu11 <linalxly@gmail.com>
2022-05-08 18:01:31 -0400

    styles change

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,25 +1,45 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import {Context as BlogContext} from "./BlogContext";
+import { EvilIcons } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const {state, addpost} = useContext(BlogContext);
   // console.log("posts " + blogPosts);
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button title=" Add Post " onPress={addpost}></Button>
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({item}) => {
-          return <Text>{item.title}</Text>;
+          return (<View style={styles.row}>
+            <Text style = {styles.title}>{item.title}</Text>
+            <EvilIcons name = "trash" style = {styles.icon}></EvilIcons>
+            </View>
+          )
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderBottomWidth:1,
+    borderColor: 'gray',
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
