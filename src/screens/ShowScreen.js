ba1ee0ac4b5e88eecb6093b2b5c8f1e5006f1ab9
LingyuLiu11 <linalxly@gmail.com>
2022-05-08 20:51:12 -0400

    add edit icon in show post screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,9 +1,29 @@
-import { View, Text, StyleSheet } from "react-native";
-import { useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useContext } from "react";
 import { Context as BlogContext } from "./BlogContext";
+import { AntDesign } from '@expo/vector-icons';
 
-const ShowScreen = ({ route: { params } }) => {
+const ShowScreen = ({ navigation, route: {params} }) => {
   const { state } = useContext(BlogContext);
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: (props) => (
+        <View>
+          <Text>Posts</Text>
+        </View>
+        
+      ),
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+          <AntDesign name="edit" size={24} color="black" />
+        </TouchableOpacity>
+      ),
+      headerStyle: {
+        backgroundColor: "#f4511e", //Set Header color
+      },
+    });
+  }, [navigation]);
   
   const post = state.find((post) => post.id === params.id);
 
